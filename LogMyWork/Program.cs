d40fa704181e2494ce2ba99c28dfa358de173992
Timo Peltoniemi <timo.peltoniemi@alfame.com>
2023-09-11 09:46:23 +0300

    Added folder structure for log files Added log filtering for office hours

--- LogMyWork/Program.cs
+++ LogMyWork/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Globalization;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Text.Json;
@@ -14,7 +15,7 @@ class Program
         {
             PropertyNameCaseInsensitive = true,
             WriteIndented = true,
-            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping // Allow non-ASCII characters
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
         };
 
         var configJson = File.ReadAllText("config.json");
@@ -25,61 +26,81 @@ class Program
 
         var applicationInfos = new List<ApplicationInfo>();
 
-        while (true) // Infinite loop
+        while (true)
         {
-            var processes = Process.GetProcesses();
+            DateTime now = DateTime.Now;
 
-            foreach (var keyword in keywords)
+            // Check if current time is within logging range
+            if (now.TimeOfDay >= TimeSpan.FromHours(8) && now.TimeOfDay <= TimeSpan.FromHours(17))
             {
-                foreach (var process in processes.Where(p => p.ProcessName == keyword && !string.IsNullOrWhiteSpace(p.MainWindowTitle)))
+                var processes = Process.GetProcesses();
+
+                foreach (var keyword in keywords)
                 {
-                    var existingApp = applicationInfos.FirstOrDefault(a => a.Name == process.ProcessName);
-                    if (existingApp == null)
+                    foreach (var process in processes.Where(p => p.ProcessName == keyword && !string.IsNullOrWhiteSpace(p.MainWindowTitle)))
                     {
-                        existingApp = new ApplicationInfo
+                        var existingApp = applicationInfos.FirstOrDefault(a => a.Name == process.ProcessName);
+                        if (existingApp == null)
                         {
-                            Name = process.ProcessName,
-                            ProcessInfos = new List<ProcessInfo>()
-                        };
+                            existingApp = new ApplicationInfo
+                            {
+                                Name = process.ProcessName,
+                                ProcessInfos = new List<ProcessInfo>()
+                            };
 
-                        applicationInfos.Add(existingApp);
-                        Console.WriteLine($"Added new application: {existingApp.Name}");
-                    }
+                            applicationInfos.Add(existingApp);
+                            Console.WriteLine($"Added new application: {existingApp.Name}");
+                        }
 
-                    var existingProcess = existingApp.ProcessInfos.FirstOrDefault(p => p.Title == process.MainWindowTitle.Replace("\t", "").Trim());
-                    if (existingProcess == null)
-                    {
-                        var info = new ProcessInfo
+                        var existingProcess = existingApp.ProcessInfos.FirstOrDefault(p => p.Title == process.MainWindowTitle.Replace("\t", "").Trim());
+                        if (existingProcess == null)
                         {
-                            Time = $"00:00:00",
-                            Title = process.MainWindowTitle.Replace("\t", "").Trim(),
-                            StartTime = DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss")
-                        };
+                            var info = new ProcessInfo
+                            {
+                                Time = $"00:00:00",
+                                Title = process.MainWindowTitle.Replace("\t", "").Trim(),
+                                StartTime = DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss")
+                            };
 
-                        existingApp.ProcessInfos.Add(info);
-                        Console.WriteLine($"Added new process: {info.Title}");
-                    }
-                    else
-                    {
-                        var time = TimeSpan.Parse(existingProcess.Time);
-                        time = time.Add(TimeSpan.FromMilliseconds(sleepDuration));
-                        existingProcess.Time = $"{time.Hours:D2}:{time.Minutes:D2}:{time.Seconds:D2}";
+                            existingApp.ProcessInfos.Add(info);
+                            Console.WriteLine($"Added new process: {info.Title}");
+                        }
+                        else
+                        {
+                            var time = TimeSpan.Parse(existingProcess.Time);
+                            time = time.Add(TimeSpan.FromMilliseconds(sleepDuration));
+                            existingProcess.Time = $"{time.Hours:D2}:{time.Minutes:D2}:{time.Seconds:D2}";
 
-                        Console.WriteLine($"Updated process: {existingProcess.Title}, New time: {existingProcess.Time}");
+                            Console.WriteLine($"Updated process: {existingProcess.Title}, New time: {existingProcess.Time}");
+                        }
                     }
                 }
+
+                var directoryPath = Path.Combine(now.Year.ToString(), now.Month.ToString(), GetWeekOfMonth(now).ToString());
+                Directory.CreateDirectory(directoryPath);
+
+                var filename = Path.Combine(directoryPath, $"{now:dd-MM-yyyy}_processes.json");
+                var json = JsonSerializer.Serialize(applicationInfos, options);
+                var jsonBytes = Encoding.UTF8.GetBytes(json);
+
+                File.WriteAllBytes(filename, jsonBytes);
+
+                Console.WriteLine($"JSON file updated: {filename}\n");
             }
 
-            var json = JsonSerializer.Serialize(applicationInfos, options);
-            var filename = $"{DateTime.Now:dd-MM-yyyy}_processes.json";
-            var jsonBytes = Encoding.UTF8.GetBytes(json);
-
-            File.WriteAllBytes(filename, jsonBytes);
-
-            Console.WriteLine($"JSON file updated: {filename}\n");
-
-            Thread.Sleep(sleepDuration); // Wait for specified duration
+            Thread.Sleep(sleepDuration);
         }
+    }
+
+    // Helper method to get week of month
+    public static int GetWeekOfMonth(DateTime date)
+    {
+        DateTime beginningOfMonth = new DateTime(date.Year, date.Month, 1);
+
+        while (date.Date.AddDays(1).DayOfWeek != CultureInfo.CurrentCulture.DateTimeFormat.FirstDayOfWeek)
+            date = date.AddDays(1);
+
+        return (int)Math.Truncate((double)date.Subtract(beginningOfMonth).TotalDays / 7f) + 1;
     }
 }
 
