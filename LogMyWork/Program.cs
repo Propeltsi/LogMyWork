ef3956ddc4f962cb73e2c5515b7159dee17bf496
Timo Peltoniemi <timo.peltoniemi@alfame.com>
2023-09-11 12:31:56 +0300

    Added bunch of new features like .json logs are replaced with sqlite

--- LogMyWork/Program.cs
+++ LogMyWork/Program.cs
@@ -1,8 +1,12 @@
-﻿using System.Diagnostics;
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
-using System.Text;
+using System.Linq;
 using System.Text.Encodings.Web;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 
 class Program
 {
@@ -11,6 +15,10 @@ class Program
 
     static void Main()
     {
+        SQLitePCL.Batteries.Init();
+
+        InitializeDatabase();
+
         var options = new JsonSerializerOptions
         {
             PropertyNameCaseInsensitive = true,
@@ -30,69 +38,173 @@ class Program
         {
             DateTime now = DateTime.Now;
 
-            // Check if current time is within logging range
             if (now.TimeOfDay >= TimeSpan.FromHours(8) && now.TimeOfDay <= TimeSpan.FromHours(17))
             {
-                var processes = Process.GetProcesses();
+                var activeProcess = GetActiveProcess();
 
-                foreach (var keyword in keywords)
+                if (keywords.Contains(activeProcess.ProcessName))
                 {
-                    foreach (var process in processes.Where(p => p.ProcessName == keyword && !string.IsNullOrWhiteSpace(p.MainWindowTitle)))
+                    // Hae ja normalisoi ikkunan otsikko
+                    var title = activeProcess.MainWindowTitle.Replace("\t", "").Trim();
+                    var normalizedTitle = NormalizeWindowTitle(title);
+
+                    var existingApp = applicationInfos.FirstOrDefault(a => a.Name == activeProcess.ProcessName);
+                    if (existingApp == null)
                     {
-                        var existingApp = applicationInfos.FirstOrDefault(a => a.Name == process.ProcessName);
-                        if (existingApp == null)
+                        existingApp = new ApplicationInfo
                         {
-                            existingApp = new ApplicationInfo
-                            {
-                                Name = process.ProcessName,
-                                ProcessInfos = new List<ProcessInfo>()
-                            };
+                            Name = activeProcess.ProcessName,
+                            ProcessInfos = new List<ProcessInfo>()
+                        };
 
-                            applicationInfos.Add(existingApp);
-                            Console.WriteLine($"Added new application: {existingApp.Name}");
-                        }
+                        applicationInfos.Add(existingApp);
+                        Console.WriteLine($"Added new application: {existingApp.Name}");
+                        InsertOrUpdateApplication(existingApp);
+                    }
 
-                        var existingProcess = existingApp.ProcessInfos.FirstOrDefault(p => p.Title == process.MainWindowTitle.Replace("\t", "").Trim());
-                        if (existingProcess == null)
+                    var existingProcess = existingApp.ProcessInfos.FirstOrDefault(p => p.Title == activeProcess.MainWindowTitle.Replace("\t", "").Trim());
+                    if (existingProcess == null)
+                    {
+                        var info = new ProcessInfo
                         {
-                            var info = new ProcessInfo
-                            {
-                                Time = $"00:00:00",
-                                Title = process.MainWindowTitle.Replace("\t", "").Trim(),
-                                StartTime = DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss")
-                            };
+                            Time = $"00:00:05",  // Alusta aika 5 sekunnilla, koska uusi prosessi havaitaan
+                            Title = normalizedTitle,
+                            StartTime = DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss")
+                        };
 
-                            existingApp.ProcessInfos.Add(info);
-                            Console.WriteLine($"Added new process: {info.Title}");
-                        }
-                        else
-                        {
-                            var time = TimeSpan.Parse(existingProcess.Time);
-                            time = time.Add(TimeSpan.FromMilliseconds(sleepDuration));
-                            existingProcess.Time = $"{time.Hours:D2}:{time.Minutes:D2}:{time.Seconds:D2}";
+                        existingApp.ProcessInfos.Add(info);
+                        Console.WriteLine($"Added new process: {info.Title}");
+                        InsertOrUpdateProcessInfo(info, existingApp.Name);
+                    }
+                    else
+                    {
+                        var time = TimeSpan.Parse(existingProcess.Time);
+                        time = time.Add(TimeSpan.FromSeconds(5)); // Lisää 5 sekuntia, ei millisekuntia
+                        existingProcess.Time = $"{time.Hours:D2}:{time.Minutes:D2}:{time.Seconds:D2}";
 
-                            Console.WriteLine($"Updated process: {existingProcess.Title}, New time: {existingProcess.Time}");
-                        }
+                        Console.WriteLine($"Updated process: {existingProcess.Title}, New time: {existingProcess.Time}");
+                        InsertOrUpdateProcessInfo(existingProcess, existingApp.Name);  // Päivitä tietokantaa uudella ajalla
                     }
                 }
-
-                var directoryPath = Path.Combine(now.Year.ToString(), now.Month.ToString(), GetWeekOfMonth(now).ToString());
-                Directory.CreateDirectory(directoryPath);
-
-                var filename = Path.Combine(directoryPath, $"{now:dd-MM-yyyy}_processes.json");
-                var json = JsonSerializer.Serialize(applicationInfos, options);
-                var jsonBytes = Encoding.UTF8.GetBytes(json);
-
-                File.WriteAllBytes(filename, jsonBytes);
-
-                Console.WriteLine($"JSON file updated: {filename}\n");
             }
 
             Thread.Sleep(sleepDuration);
         }
     }
 
-    // Helper method to get week of month
+    public static void InitializeDatabase()
+    {
+        using (var connection = new SqliteConnection("Data Source=processes.db"))
+        {
+            connection.Open();
+
+            string createApplicationTable = @"
+            CREATE TABLE IF NOT EXISTS Applications (
+                Id INTEGER PRIMARY KEY,
+                Name TEXT NOT NULL
+            );";
+
+            string createProcessInfoTable = @"
+            CREATE TABLE IF NOT EXISTS ProcessInfos (
+                Id INTEGER PRIMARY KEY,
+                Time TEXT NOT NULL,
+                Title TEXT NOT NULL,
+                StartTime TEXT NOT NULL,
+                ApplicationId INTEGER,
+                FOREIGN KEY(ApplicationId) REFERENCES Applications(Id)
+            );";
+
+            using (var command = new SqliteCommand(createApplicationTable, connection))
+            {
+                command.ExecuteNonQuery();
+            }
+
+            using (var command = new SqliteCommand(createProcessInfoTable, connection))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+
+    public static void InsertOrUpdateApplication(ApplicationInfo appInfo)
+    {
+        using (var connection = new SqliteConnection("Data Source=processes.db"))
+        {
+            connection.Open();
+
+            var cmdCheck = new SqliteCommand($"SELECT Id FROM Applications WHERE Name = '{appInfo.Name}'", connection);
+            var result = cmdCheck.ExecuteScalar();
+            if (result != null)
+            {
+                // (If needed, add logic to update the existing application)
+            }
+            else
+            {
+                var cmdInsert = new SqliteCommand($"INSERT INTO Applications (Name) VALUES ('{appInfo.Name}')", connection);
+                cmdInsert.ExecuteNonQuery();
+            }
+        }
+    }
+
+    public static void InsertOrUpdateProcessInfo(ProcessInfo pInfo, string appName)
+    {
+        using (var connection = new SqliteConnection("Data Source=processes.db"))
+        {
+            connection.Open();
+
+            var cmdGetAppId = new SqliteCommand($"SELECT Id FROM Applications WHERE Name = '{appName}'", connection);
+            var appId = cmdGetAppId.ExecuteScalar();
+
+            if (appId != null)
+            {
+                var cmdCheck = new SqliteCommand($"SELECT Id FROM ProcessInfos WHERE Title = '{pInfo.Title}' AND ApplicationId = {appId}", connection);
+                var result = cmdCheck.ExecuteScalar();
+                if (result != null)
+                {
+                    // Update the existing process info
+                    var cmdUpdate = new SqliteCommand($"UPDATE ProcessInfos SET Time = '{pInfo.Time}' WHERE Id = {result}", connection);
+                    cmdUpdate.ExecuteNonQuery();
+                }
+                else
+                {
+                    var cmdInsert = new SqliteCommand($"INSERT INTO ProcessInfos (Time, Title, StartTime, ApplicationId) VALUES ('{pInfo.Time}', '{pInfo.Title}', '{pInfo.StartTime}', {appId})", connection);
+                    cmdInsert.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+
+    public static string NormalizeWindowTitle(string title)
+    {
+        // Poista suluissa olevat osat, kuten "Inbox (1)"
+        var cleanTitle = Regex.Replace(title, @"\(\d+\)", "").Trim();
+
+        // Poista "x new item" tai vastaavat
+        cleanTitle = Regex.Replace(cleanTitle, @"\d+ new item[s]*", "", RegexOptions.IgnoreCase).Trim();
+
+        // Poista ylimääräiset välilyönnit
+        cleanTitle = Regex.Replace(cleanTitle, @"\s+", " ");
+
+        // Poista ylimääräiset "- -"
+        cleanTitle = Regex.Replace(cleanTitle, @"- -", "-").Trim();
+
+        return cleanTitle;
+    }
+
+    private static Process GetActiveProcess()
+    {
+        IntPtr hwnd = GetForegroundWindow();
+        uint pid;
+        GetWindowThreadProcessId(hwnd, out pid);
+        return Process.GetProcessById((int)pid);
+    }
+
+    [System.Runtime.InteropServices.DllImport("user32.dll")]
+    private static extern IntPtr GetForegroundWindow();
+
+    [System.Runtime.InteropServices.DllImport("user32.dll")]
+    private static extern uint GetWindowThreadProcessId(IntPtr hwnd, out uint lpdwProcessId);
+
     public static int GetWeekOfMonth(DateTime date)
     {
         DateTime beginningOfMonth = new DateTime(date.Year, date.Month, 1);
@@ -102,23 +214,23 @@ class Program
 
         return (int)Math.Truncate((double)date.Subtract(beginningOfMonth).TotalDays / 7f) + 1;
     }
-}
 
-public class ProcessInfo
-{
-    public string Time { get; set; }
-    public string Title { get; set; }
-    public string StartTime { get; set; }
-}
+    public class ProcessInfo
+    {
+        public string Time { get; set; }
+        public string Title { get; set; }
+        public string StartTime { get; set; }
+    }
 
-public class ApplicationInfo
-{
-    public string Name { get; set; }
-    public List<ProcessInfo> ProcessInfos { get; set; }
-}
+    public class ApplicationInfo
+    {
+        public string Name { get; set; }
+        public List<ProcessInfo> ProcessInfos { get; set; }
+    }
 
-public class Config
-{
-    public List<string> Keywords { get; set; }
-    public int SleepDuration { get; set; }
+    public class Config
+    {
+        public List<string> Keywords { get; set; }
+        public int SleepDuration { get; set; }
+    }
 }
